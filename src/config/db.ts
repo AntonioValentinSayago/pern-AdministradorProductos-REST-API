b00952cfe51f77d0b0fe8a844c70bfb651a46776
AntonioValentinSayago <cesar_anto30@hotmail.com>
2025-01-14 21:02:30 -0600

    Instalacion de sequelize para el ORM de la base de datos con render

---
+++ src/config/db.ts
@@ -0,0 +1,5 @@
+import { Sequelize } from 'sequelize';
+
+const db = new Sequelize('postgresql://rest_api_node_typescript_z281_user:DyajlvfSTcdw80KO7Myd1cRGTKy6o8eI@dpg-cu3i0hrqf0us73b0lsv0-a.oregon-postgres.render.com/rest_api_node_typescript_z281');
+
+export default db
