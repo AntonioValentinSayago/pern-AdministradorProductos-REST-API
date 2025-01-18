ad5e075e8a36f703c29e5cf63f4984d86bb5c4c6
AntonioValentinSayago <cesar_anto30@hotmail.com>
2025-01-18 16:46:03 -0600

    Endpoint de getProducts and createProducts

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { body, param } from "express-validator"; // Check en operacione asincronas y en body en caso contrario
-import { createProducts, getProducts, getProductsById } from "./handlers/products";
+import { createProducts, getProducts, getProductsById, updateProducts } from "./handlers/products";
 import { handleInputErrors } from "./middleware";
 
 const router = Router();
@@ -25,9 +25,19 @@ router.post('/',
     createProducts
 )
 
-router.put('/', (req, res) => {
-    res.json('Hello Put')
-})
+router.put('/:id',
+    // Validar datos
+    body('name')
+        .notEmpty().withMessage('Name is required'),
+        // .run(req); Solamente en operaciones asincronas
+    body('price', 'Price is required')
+        .notEmpty().withMessage('Price is required')
+        .isNumeric().withMessage('Value is Valid')
+        .custom(value => value > 0).withMessage('Price must be greater than 0'),
+    body('availability').withMessage('Availability is not valid'),
+    handleInputErrors,
+    updateProducts 
+)
 
 router.delete('/', (req, res) => {
     res.json('Hello Delete')
