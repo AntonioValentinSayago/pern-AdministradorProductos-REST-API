fe390bf4c0f0a4f827417e9d2760118f8dca6c74
AntonioValentinSayago <cesar_anto30@hotmail.com>
2025-01-27 20:19:08 -0600

    Archivo de test para el servidor con express

--- src/__test__/server.test.ts
+++ src/__test__/server.test.ts
@@ -1,5 +1,13 @@
-describe('', () => { 
-    test('', () => {
-        expect(1).toBe(1)
+import request from 'supertest'
+import server from '../server'
+
+describe('GET /api', () => { 
+    it('Should sen back a json response', async () => {
+        const response = await request(server).get('/api')
+        expect(response.status).toBe(200)
+        expect(response.headers['content-type']).toEqual(expect.stringContaining('json'))
+        expect(response.headers['content-type']).toMatch(/json/)
+
+        expect(response.status).not.toBe(404)
     })
  })
