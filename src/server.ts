49d4fb418932993fc82fff5ef1ae260324e38763
AntonioValentinSayago <cesar_anto30@hotmail.com>
2025-02-06 18:35:53 -0600

    Configuracion del endpoint para deployar Swagger

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import express from 'express'
 import colors from 'colors'
 import router  from './router'
 import db from './config/db'
+import swaggerUi from 'swagger-ui-express'
+import swaggerSpec from './config/swagger'
 
 // Conectar a base de datos
 export async function connectDB() {
@@ -24,8 +26,7 @@ server.use(express.json())
 
 server.use('/api/products', router)
 
-server.get('/api', (req, res) => {
-    res.json({msg: 'Desde API'})
-})
+// Docs de Swagger
+server.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 
 export default server
