828d17b2aa51e15de89bc1ac7d10566aea5da042
AntonioValentinSayago <cesar_anto30@hotmail.com>
2025-02-13 14:12:46 -0600

    Se agrega el action y loader a la funcion para editar productos

--- client-pern-products-frontend/src/router.tsx
+++ client-pern-products-frontend/src/router.tsx
@@ -1,22 +1,30 @@
 import { createBrowserRouter } from 'react-router-dom'
 import Layout from './layouts/Layout'
-import { Products } from './views/Products'
+import { Products, loader as productsLoader } from './views/Products'
 import { NewProducts, action as newProductAction } from './views/NewProducts'
+import EditProduct, { loader as editProductLoader, action as editProductAction} from './views/EditProduct'
 
 export const router = createBrowserRouter([
     {
         path: '/',
         element: <Layout />,
-        children:[
+        children: [
             {
                 index: true,
-                element: <Products />
+                element: <Products />,
+                loader: productsLoader
             },
             {
                 path: 'productos/nuevo',
                 element: <NewProducts />,
                 action: newProductAction
-            }
+            },
+            {
+                path: 'productos/:id/editar', // ROA Pattern - Resource-oriented design
+                element: <EditProduct />,
+                loader: editProductLoader,
+                action: editProductAction
+            },
         ]
     }
 ])
